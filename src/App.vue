891a86bb20e1f7fef135b4006da5f2569a6fb721
Buddha1412 <tthnadchay@gmail.com>
2025-05-26 18:19:31 +0700

    display the login page on App

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
+<div>
+  
   <router-view v-slot="{ Component, route }">
     <component :is="Component" :key="route.path"/>
   </router-view>
+</div>
 </template>
 
 <script setup>
-import { watch } from 'vue';
+import { onMounted, watch, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import liff from '@line/liff';
 
 const route = useRoute();
+const nickname = ref('');
 
 watch(
   () => route.name,
@@ -30,5 +35,13 @@ watch(
   },
   { immediate: true }
 );
+
+onMounted(async () => {
+  if (liff.isLoggedIn()) {
+    const profile = await liff.getProfile()
+    nickname.value = profile.displayName;
+    }
+})
+  
 </script>
 
