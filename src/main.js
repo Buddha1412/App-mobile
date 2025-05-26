a9ddde2ac89358b8243737f26bd911da4fbe4aa3
Buddha1412 <tthnadchay@gmail.com>
2025-05-26 18:07:21 +0700

    add line-liff navigate user to log in page

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import UserForm from './views/UserForms.vue'
 import '@vuepic/vue-datepicker/dist/main.css'
-// import VueDatePicker from '@vuepic/vue-datepicker';
 import router from './router';
+import liff from '@line/liff'
 
 const app = createApp(App)
-app.use(router)
-// UserForm.component('VueDatePicker', VueDatePicker)
-.mount('#app')
+const liffId = '2007300744-2OQRxQ4Z'
+
+liff.init({ liffId })
+  .then(() => {
+    if (!liff.isLoggedIn()) {
+      liff.login();
+    } else {
+    app.use(router)
+      app.mount('#app');
+    }
+  })
+  .catch((error) => {
+    console.error('LIFF initialization failed', error);
+  });
