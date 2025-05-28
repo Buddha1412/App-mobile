19fdf6c480f13aa135dd7781a33ef630f0b18386
Buddha1412 <tthnadchay@gmail.com>
2025-05-28 11:22:39 +0700

    move the file to backend server

---
+++ src/vue-server/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import '@vuepic/vue-datepicker/dist/main.css'
+import router from '../router';
+import liff from '@line/liff'
+
+const app = createApp(App)
+const liffId = '2007300744-2OQRxQ4Z'
+
+liff.init({ liffId })
+  .then(() => {
+    if (!liff.isLoggedIn()) {
+      liff.login();
+    } else {
+    app.use(router)
+      app.mount('#app');
+    }
+  })
+  .catch((error) => {
+    console.error('LIFF initialization failed', error);
+  });
